51eae70ad3b9a61c4a47ca71361c16814d36a476
Hasan99722 <153342687+Hasan99722@users.noreply.github.com>
2025-05-01 17:32:14 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Variables
-IMAGE_NAME=testapp
-ECR_URI=762233754891.dkr.ecr.us-east-1.amazonaws.com
-AWS_REGION=us-east-1
-CONTAINER_NAME=nginx-server
+IMAGE_NAME=ai-server-image
+ECR_URI=014498634556.dkr.ecr.ap-south-1.amazonaws.com/akhil-ai-server
+AWS_REGION=ap-south-1
+CONTAINER_NAME=ai-server
 
 echo "Logging in to ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
@@ -19,6 +19,6 @@ echo "Pulling latest image..."
 docker pull $ECR_URI/$IMAGE_NAME:latest
 
 echo "Starting new container..."
-docker run -d --name $CONTAINER_NAME -p 80:3000 $ECR_URI/$IMAGE_NAME:latest
+docker run -d --name $CONTAINER_NAME -p 8000:8000 $ECR_URI/$IMAGE_NAME:latest
 
 echo "Deployment complete."
